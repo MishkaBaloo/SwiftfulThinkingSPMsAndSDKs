515d6b58c9f2048f62d35c0e380513557d9dff9c
Mykhailo Tokmakov <misha.tokmak@gmail.com>
2025-01-31 22:39:10 +0200

    Initial Commit

---
+++ SwiftfulThinkingSPMsAndSDKs/SwiftfulThinkingSPMsAndSDKsApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftfulThinkingSPMsAndSDKsApp.swift
+//  SwiftfulThinkingSPMsAndSDKs
+//
+//  Created by Michael on 1/31/25.
+//
+
+import SwiftUI
+
+@main
+struct SwiftfulThinkingSPMsAndSDKsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ SwiftfulThinkingSPMsAndSDKsTests/SwiftfulThinkingSPMsAndSDKsTests.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftfulThinkingSPMsAndSDKsTests.swift
+//  SwiftfulThinkingSPMsAndSDKsTests
+//
+//  Created by Michael on 1/31/25.
+//
+
+import Testing
+@testable import SwiftfulThinkingSPMsAndSDKs
+
+struct SwiftfulThinkingSPMsAndSDKsTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
